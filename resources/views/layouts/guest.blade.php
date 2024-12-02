a4e3180056ae9d9b94caba089f69dc053b8ea800
MyProgramings <reader1999a@gmail.com>
2024-12-02 11:42:53 +0300

    joj

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <link rel="stylesheet" href="{{ asset('build/assets/app.9e926a85.css') }}">
+        <script src="{{ asset('build/assets/app.d225c007.js') }}"></script>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
